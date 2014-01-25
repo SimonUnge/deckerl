129936b8c4c266b231a90217b32706937cac29ad
Simon Unge <su@tail-f.com>
2014-01-25 16:29:43 +0530

    Using sort/2

--- src/deck_util.erl
+++ src/deck_util.erl
@@ -23,12 +23,31 @@ draw_N_cards(N, Deck) ->
     lists:split(N, Deck).
 
 sort_deck(Deck) ->
-    Hearts = [ Card || Card <- Deck, element(1, Card) =:= hearts ],
-    Spades = [ Card || Card <- Deck, element(1, Card) =:= spades ],
-    Diamonds = [ Card || Card <- Deck, element(1, Card) =:= diamonds ],
-    Clubs = [ Card || Card <- Deck, element(1, Card) =:= clubs ],
-    lists:sort(Hearts) ++ lists:sort(Spades) ++ lists:sort(Diamonds)
-    ++ lists:sort(Clubs).
+    %% Hearts = extract_suit(hearts, Deck),
+    %% Spades = extract_suit(spades, Deck),
+    %% Diamonds = extract_suit(diamonds, Deck),
+    %% Clubs = extract_suit(clubs, Deck),
+    %% lists:sort(Hearts) ++ lists:sort(Spades) ++ lists:sort(Diamonds)
+    %% ++ lists:sort(Clubs).
+    lists:sort(fun is_card_lower/2, Deck).
+
+%% extract_suit(Suit, Deck) ->
+%%     [ Card || Card <- Deck, element(1, Card) =:= Suit ].
+
+is_card_lower({Suit, Value}, {Suit, Value2}) ->
+    Value =< Value2;
+is_card_lower({hearts, _}, _) ->
+    true;
+is_card_lower({clubs, _}, _) ->
+    false;
+is_card_lower({_,_}, {hearts,_}) ->
+    false;
+is_card_lower({_,_}, {clubs, _}) ->
+    true;
+is_card_lower({spades,_}, _) ->
+    true;
+is_card_lower(_,_) ->
+    false.
 
 shuffle_deck(List) ->
     randomize(round(math:log(length(List)) + 0.5), List).
